47d53dc013d59ffce9293a66c54bd01e46f59d5f
Yurii-maker <yuri19961204@gmail.com>
2021-11-29 17:27:40 +0200

    all tasks are done

---
+++ lesson-1/errors.js
@@ -0,0 +1,14 @@
+let a = 100;
+let b = 500;
+let c = "hello";
+let d = "okten";
+let x = 'constant value';
+{
+    console.log(a);
+    x = 'new value';
+}
+b = 'new value';
+
+let result = 100 + 'y';
+
+console.log(result);
