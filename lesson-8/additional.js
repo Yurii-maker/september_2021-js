5296bbd4e515735054329336cb8c9e5ee5011003
Yurii-maker <yuri19961204@gmail.com>
2021-11-30 19:41:07 +0200

    all tasks are done

---
+++ lesson-8/additional.js
@@ -0,0 +1,23 @@
+// написати рекурсивну функцію, яка збирає всі назви класів з файлу rules.html в окремий масив. масив вивести в консоль
+
+// let arrayOfClasses = [];
+//
+// function reCall(obj) {
+//
+//     if (obj.children.length) {
+//
+//         for (const element of obj.children) {
+//             if (element.classList.length > 0) {
+//                 arrayOfClasses.push(element.classList.value);
+//
+//             }
+//             reCall(element)
+//         }
+//
+//     }
+//
+//
+// }
+//
+// reCall(document.body);
+// console.log(arrayOfClasses)
